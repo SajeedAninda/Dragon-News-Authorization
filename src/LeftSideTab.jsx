9f8a366357d29f9837a92ae6ed4c5d6a9912285d
SajeedAninda <sajaninda@gmail.com>
2023-10-03 15:49:18 +0600

    Created Left Navbar

--- src/LeftSideTab.jsx
+++ src/LeftSideTab.jsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import first from "../src/assets/1.png";
+import second from "../src/assets/2.png";
+import third from "../src/assets/3.png";
+import { format } from 'date-fns';
+import { AiOutlineCalendar } from 'react-icons/ai';
+
+
 
 const LeftSideTab = () => {
+    let [categories, setCategories] = useState([]);
+    const currentDate = new Date();
+    const formattedDate = format(currentDate, "MMMM d");
+
+    useEffect(() => {
+        fetch("/categories.json")
+            .then(res => res.json())
+            .then(data => setCategories(data));
+    }, [])
+
+
     return (
         <div>
-            
+            <div className='space-y-2 mt-10'>
+                <h2 className='text-xl font-bold'>All Category</h2>
+                {
+                    categories.map(category => <Link to={`category/category-${category.id}`} className='flex flex-col text-center text-lg text-[#9F9F9F] font-semibold' key={category.id}><p className='my-3 bg-gray-100 rounded-xl py-3'>{category.name}</p></Link>)
+                }
+            </div>
+
+            <div className='mt-10'>
+                <div className='space-y-2'>
+                    <img className='w-full rounded-lg' src={first} alt="" />
+                    <h3 className='text-[#403F3F] text-lg font-semibold'>Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                    <div className='flex justify-between'>
+                        <h5>Sports</h5>
+                        <div className='flex justify-center items-center gap-2'>
+                            <AiOutlineCalendar className='text-lg'></AiOutlineCalendar>
+                            <h5>{formattedDate}</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='space-y-2 mt-6'>
+                    <img className='w-full rounded-lg' src={second} alt="" />
+                    <h3 className='text-[#403F3F] text-lg font-semibold'>Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                    <div className='flex justify-between'>
+                        <h5>Sports</h5>
+                        <div className='flex justify-center items-center gap-2'>
+                            <AiOutlineCalendar className='text-lg'></AiOutlineCalendar>
+                            <h5>{formattedDate}</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='space-y-2 mt-6'>
+                    <img className='w-full rounded-lg' src={third} alt="" />
+                    <h3 className='text-[#403F3F] text-lg font-semibold'>Bayern Slams Authorities Over Flight Delay to Club World Cup</h3>
+                    <div className='flex justify-between'>
+                        <h5>Sports</h5>
+                        <div className='flex justify-center items-center gap-2'>
+                            <AiOutlineCalendar className='text-lg'></AiOutlineCalendar>
+                            <h5>{formattedDate}</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/RightSideTab.jsx
+++ src/RightSideTab.jsx
@@ -7,6 +7,7 @@ import { FaXTwitter } from 'react-icons/fa6';
 import img1 from "../src/assets/qZone1.png"
 import img2 from "../src/assets/qZone2.png"
 import img3 from "../src/assets/qZone3.png"
+import './right.css';
 
 
 const RightSideTab = () => {
@@ -40,11 +41,23 @@ const RightSideTab = () => {
                 </div>
             </div>
 
-                <h2 className='text-lg font-bold mt-10'>Q Zone</h2>
+            <h2 className='text-lg font-bold mt-10'>Q Zone</h2>
             <div className='space-y-2 mt-4 bg-gray-100 flex flex-col justify-center items-center'>
                 <img src={img1} alt="" />
                 <img src={img2} alt="" />
                 <img src={img3} alt="" />
+            </div>
+
+            <div className='text-center flex justify-center mt-10  py-12 bg'>
+                <div className='w-2/3 space-y-4'>
+                    <h1 className='text-[30px] font-bold text-white'>Create Amazing Newspaper</h1>
+                    <p className='text-white'>
+                        Discover thousands of options, easy to customize layouts, one-click to import demo and much more.
+                    </p>
+                    <div className='flex justify-center items-center'>
+                        <button className='py-3 px-3 text-white bg-[#D72050]'>Learn More</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
