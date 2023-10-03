6c55efd19d4dc61b30777764f8ff2feba3e3f8fc
SajeedAninda <sajaninda@gmail.com>
2023-10-03 13:26:08 +0600

    Added current date with date-fns

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from "../assets/logo.png"
+import { format } from 'date-fns';
 
 const Header = () => {
+    const currentDate = new Date();
+    const formattedDate = format(currentDate, "EEEE, MMMM d, yyyy");
+
     return (
-        <div className='flex flex-col justify-center items-center my-6'>
+        <div className='flex flex-col justify-center items-center my-6 space-y-2'>
             <img className='w-[350px]' src={logo} alt="" />
-            <p>Journalism Without Fear or Favour</p>
-            <h4>Sunday, November 27, 2022</h4>
+            <p className='text-[#706F6F]'>Journalism Without Fear or Favour</p>
+            <h4>{formattedDate}</h4>
         </div>
     );
 };
